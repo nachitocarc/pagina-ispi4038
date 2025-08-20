3edc230fc6ae0ede636bd43b65f473105f6b26be
Edgardo Soto <131820241+EdgardoSoto1@users.noreply.github.com>
2025-08-20 16:27:19 -0300

    conexion api e iconos
    funcion js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,66 @@
-function mostrarInformacionAcademica() {
-    let info = document.getElementById("infoAcademica");
-    if(info.style.display === "none") {
-        info.style.display = "block";
-    } else {
-        info.style.display = "none";
+const apiUrl = "https://dolarapi.com/v1/cotizaciones";
+const content = document.getElementById("cotizaciones");
+
+async function consultarCotizacion() {
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      content.innerHTML = "";
+      for (const dat of data) {
+        content.innerHTML += `
+          <div class="cotizacion">
+            <div>
+              <a href="#" class="logo-cotizacion">
+                <i class="fa-solid fa-hand-holding-dollar"></i>
+              </a>
+            </div>
+            <div class="par">${dat.moneda}</div>
+            <div id="compra-venta">
+              <div>
+                <h3>Compra</h3>
+                <div class="valor">${dat.compra}</div>
+              </div>
+              <div>
+                <h3>Venta</h3>
+                <div class="valor">${dat.venta}</div>
+              </div>
+            </div>
+            <div class="variacion"></div>
+          </div>
+        `;
+      }
     }
-}
-function mostrarInformacionInstitucional() {
-    let info = document.getElementById("infoInstitucional");
-    if(info.style.display === "none") {
-        info.style.display = "block";
-    } else {
-        info.style.display = "none";
-    }
+  } catch (error) {
+    console.error("Error al consultar la API:", error);
+  }
 }
 
-function mostrarmsj(){
-    alert("hola milagro")
+
+
+
+function mostrarInformacionAcademica() {
+  let info = document.getElementById("infoAcademica");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
 }
+function mostrarInformacionInstitucional() {
+  let info = document.getElementById("infoInstitucional");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
+}
+
+function mostrarmsj() {
+  alert("hola milagro");
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  consultarCotizacion();
+});
